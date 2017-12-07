43259009f51cc224ca24100ca220b34d339dd8a5
Jason Heard <jasonh@pandell.com>
2017-12-07 14:47:42 -0700

    Initial commit; 3 days done

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<Tuple<T, T>> AllPairs<T>(this IEnumerable<T> input)
+        {
+            var inputArray = input.ToArray();
+            
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                for (var j = i + 1; j < inputArray.Length; j++)
+                {
+                    yield return Tuple.Create(inputArray[i], inputArray[j]);
+                }
+            }
+        }
+
+        public static IEnumerable<T> Debug<T>(this IEnumerable<T> input, string label = null, Func<T, string> printer = null)
+        {
+            foreach (var item in input)
+            {
+                Console.WriteLine($"{label ?? "Got value"}: '{(printer == null ? item.ToString() : printer(item))}'");
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<T> Generate<T>(T seed, Func<T, bool> predicate, Func<T, T> iterate)
+        {
+            var next = seed;
+            while (predicate(next))
+            {
+                yield return next;
+                next = iterate(next);
+            }
+        }
+
+        public static IEnumerable<Tuple<T, T>> PairWise<T>(this IEnumerable<T> input)
+        {
+            var last = default(T);
+            var first = true;
+            foreach (var next in input)
+            {
+                if (!first)
+                {
+                    yield return Tuple.Create(last, next);
+                }
+                last = next;
+                first = false;
+            }
+        }
+
+        public static IEnumerable<T> StartWith<T>(this IEnumerable<T> input, T item)
+        {
+            return new[] {item}
+                .Concat(input);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Advent of Code 2017
+
+Solutions to the [Advent of Code 2017](http://adventofcode.com/2017) using .NET Core 2.0.
+
+## Dependencies
+
+This library is based on .NET Core 2.0.  It should work on Windows, Linux and OS X.
+
+Required tools:
+
+- [.NET Core >=2.0.3](https://www.microsoft.com/net/download) - For website
+  extraction.
+
+Used libraries (automatically installed):
+
+- System.Collections.Immutable - Because immutability is fun!
+- JetBrains.Annotations - To improve code quality analysis.
+
+## Running
+
+To run the project:
+
+1. Install .NET Core.
+1. Download the code.
+1. Run `dotnet run <day>`.
